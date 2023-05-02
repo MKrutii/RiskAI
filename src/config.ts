a6163ff09b3cc60b50f9e25791cd98dd1b506204
MKrutii <archailectmyst@gmail.com>
2023-05-02 12:56:48 -0400

    Update JSON server config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const ENV: string = process.env.NEXT_PUBLIC_ENV || 'development'
 
 export const API = {
-  development: 'http://localhost:3001',
-  production: 'test',
+  development: 'http://127.0.0.1:3001',
+  production: 'https://risk-wiz-server.vercel.app',
 }[ENV]
